a526099672973f8c459e7d6f3a208ec2dfb36cba
Christian Lisangola <jochri3@gmail.com>
2022-07-05 13:18:48 +0300

    2.Datatype

---
+++ src/Datatypes.java
@@ -0,0 +1,23 @@
+public class Datatypes {
+    public static void main(String[] args) {
+//        Chaine de caractères
+        String prenom="Christian";
+        String nom="Lisangola";
+
+//        Caractère
+        char genre='m';
+
+//        Réels
+//        Poids : 85.32746832643467322
+//        Prix : 45.78698976869
+        float poids=85.5F;
+        float tjm=8.4F;
+
+//        Réel double précision
+        double v=45.3;
+
+//        Byte : une variante d'entier court
+        byte age=89;
+
+    }
+}
