56ac56ab9ba2a22854774ebd5f47b86b1aced4a2
Christian Lisangola <jochri3@gmail.com>
2022-07-11 14:57:21 +0300

    24.Gestion des exceptions

--- src/FunctionsArray.java
+++ src/FunctionsArray.java
@@ -4,7 +4,10 @@ public class FunctionsArray {
     // {1,2,3,4,5}
     // { 6,7,8,9,10}
     // { 7,9,11,13,15 }
-    static int[] produceSumArray(int[] tableauNombres1,int[] tableauNombres2){
+    static int[] produceSumArray(int[] tableauNombres1,int[] tableauNombres2) throws Exception {
+        if(tableauNombres1.length!=tableauNombres2.length){
+            throw new Exception("Les 2 tableaux n'ont pas la même taille");
+        }
         int taille=tableauNombres1.length;
         int[] tableauResultant=new int[taille];
         for(int i=0;i<taille;i++){
@@ -13,9 +16,15 @@ public class FunctionsArray {
         return tableauResultant;
     }
     public static void main(String[] args) {
+        System.out.println("Début de l'execution");
         int[] tb1={1,2,3,4,5};
-        int[] tb2={ 6,7,8,9,10};
-        int[] tbResultant=produceSumArray(tb1,tb2);
-        System.out.println(Arrays.toString(tbResultant));
+        int[] tb2={ 6,7,8,9};
+        try{
+            int[] tbResultant=produceSumArray(tb1,tb2);
+            System.out.println(Arrays.toString(tbResultant));
+        }catch (Exception err){
+            System.out.println("Erreur => "+err);
+        }
+
     }
 }
