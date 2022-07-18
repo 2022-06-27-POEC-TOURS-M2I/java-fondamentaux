e3b079bf42d6ad1a4dbdb9329b9550bc8d19dcc9
Christian Lisangola <jochri3@gmail.com>
2022-07-18 13:34:26 +0300

    38.Interfaces Intro

---
+++ src/interfaces/Actions.java
@@ -0,0 +1,13 @@
+package interfaces;
+
+public class Actions {
+//   Ces 2 codes font exactement la même chose
+    public void presenterUnSuperHerosVolant(SuperHerosVolant superHerosVolant){
+        //Instructions
+    }
+
+    public void presenterUnInsectVolant(MechantInsecte mechantInsecte){
+        //Instuctions
+    }
+
+}
---
+++ src/interfaces/Main.java
@@ -0,0 +1,9 @@
+package interfaces;
+
+public class Main {
+    public static void main(String[] args) {
+        var superman=new SuperHerosVolant("Clark Kent","23/09/1889",500,1000);
+        var flash=new SuperHerosCourant("Barry Allen","03/05/1890",300,200);
+        var insecte=new MechantInsecte("Jeff Goldblum","05/02/1790");
+    }
+}
